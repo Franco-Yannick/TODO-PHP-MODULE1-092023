1aec85e35d0362df2c9bb845ae2a7efb2968c60e
Franco-Yannick <franco.yannick@yahoo.com>
2023-10-17 17:37:52 +0300

    protet TODO list Sayna

---
+++ config/app.php
@@ -0,0 +1,3 @@
+<?php
+    const APP_NAME = 'Super to do list';
+    const FILE_NAME = 'items.txt';
---
+++ fct/request.php
@@ -0,0 +1,8 @@
+<?php
+    function post($name, $default=null){
+        if(isset($_POST[$name])){
+            return htmlentities( $_POST[$name]);
+        }else{
+            return $default;
+        }
+    }
